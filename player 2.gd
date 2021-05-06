5b021f1be4161ae51710fe94a46d678fbc76c759
JT <jan.torge.schneider@gmail.com>
2021-05-06 16:13:11 +0200

    Unabhängige Steuerung für Spieler 1 und 2

--- player 2.gd
+++ player 2.gd
@@ -15,22 +15,22 @@ const FLOOR_NORMAL: = Vector2.UP
 func _physics_process(delta:float ) -> void:
 	
 	#Check if the player let go of the jump button
-	var is_jump_interrupted: = Input.is_action_just_released("up") and _velocity.y < 0.0
+	var is_jump_interrupted: = Input.is_action_just_released("ui_up") and _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 	
 	#Changing Sprites for left and right
-	if Input.is_action_just_pressed("left"):
+	if Input.is_action_just_pressed("ui_left"):
 		$AnimatedSprite.flip_h = true
-	elif Input.is_action_just_pressed("right"):
+	elif Input.is_action_just_pressed("ui_right"):
 		$AnimatedSprite.flip_h = false
 
 #Check which keys the player is pressing to move
 func get_direction() -> Vector2:
 	return Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"), 
-		-1.0 if Input.is_action_just_pressed("up") and is_on_floor() else 0.0
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 
+		-1.0 if Input.is_action_just_pressed("ui_up") and is_on_floor() else 0.0
 	)
 	
 func calculate_move_velocity(
